fab77e30bece871e3491e653fe5674bebe3f5514
Nosi-Msomi <103424788+Nosi-Msomi@users.noreply.github.com>
2023-10-24 21:17:06 +0200

    Initial Commit
    Adding initial dashboard app

---
+++ Dashboard/charts.py
@@ -0,0 +1,36 @@
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import pandas as pd
+
+df = pd.read_csv("rsa_cases_vs_levels.csv")
+epiweek = df.iloc[-1, 8]
+no_cases = df.iloc[-1, 1]
+end_week = df.iloc[-1, -1]
+
+def bar_chart(df):
+
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    fig.add_trace(
+        go.Bar(
+            x=df['epiweek2'], y=df['n'],
+            marker_color='gray',
+            name="Clinical Cases"),
+        secondary_y=False)  # specify for colour for df
+
+    fig.add_trace(
+        go.Scatter(
+            x=df['epiweek2'], y=df['sum_genomes'],
+            line=dict(color="blue", width=4),
+            text=df['sum_genomes'],
+            name="Wastewater levels"), secondary_y=True)
+
+    fig.update_layout(
+        title=' South African SARS-CoV-2 Wastewater Levels',
+        barmode='group')
+
+    fig.update_xaxes(title_text="Epidemiological week")
+    fig.update_yaxes(title_text="Laboratory confirmed cases", secondary_y=False)
+    fig.update_yaxes(title_text="Log Genome Copies/ml (N Gene)", secondary_y=True)
+
+    return fig
---
+++ Dashboard/navbar.py
@@ -0,0 +1,43 @@
+from dash import html
+import dash_bootstrap_components as dbc
+
+search_bar = dbc.Row(
+    [
+        dbc.Col(dbc.Input(type="search", placeholder="Search")),
+        dbc.Col(
+            dbc.Button(
+                "Search", color="primary", className="ms-2", n_clicks=0
+            ),
+            width="auto",
+        ),
+    ],
+    className="g-0 ms-auto flex-nowrap mt-3 mt-md-0",
+    align="center",
+)
+
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            html.A(
+                dbc.Row(
+                    [
+                        dbc.Col(dbc.NavbarBrand("NICD Wastewater Genomics", className="ms-2")),
+                    ],
+                    align="center",
+                    className="g-0",
+                ),
+                href="/",
+                style={"textDecoration": "none"},
+            ),
+            dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
+            dbc.Collapse(
+                search_bar,
+                id="navbar-collapse",
+                is_open=False,
+                navbar=True,
+            ),
+        ]
+    ),
+    color="dark",
+    dark=True,
+)
